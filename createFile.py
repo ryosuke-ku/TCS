4e55c970da77548cea9249a90239763d89da832f
Kurachi <naist1020@gmail.com>
2019-10-24 11:14:52 +0900

    first commit

---
+++ createFile.py
@@ -0,0 +1,46 @@
+import glob
+import os
+import re
+
+
+def printProductionPath(project_name):
+    initTestPath1 = glob.glob('C:\\Users\\ryosuke-ku\\Desktop\\' + project_name + '\\**\\*Test.java', recursive=True)
+    productionPath1 =[]
+    for initTestPath in initTestPath1:
+        testProjectName = re.sub(r"C:\\Users\\ryosuke-ku\\Desktop\\", "", initTestPath)[re.sub(r"C:\\Users\\ryosuke-ku\\Desktop\\", "", initTestPath).find("\\") + 1:][:re.sub(r"C:\\Users\\ryosuke-ku\\Desktop\\", "", initTestPath)[re.sub(r"C:\\Users\\ryosuke-ku\\Desktop\\", "", initTestPath).find("\\") + 1:].find('\\')]
+        # print(testProjectName)
+        testFileName = initTestPath[initTestPath.rfind("\\") + 1:]
+        fileName = testFileName.replace('Test','').replace('test','')
+        prodPath1 = glob.glob('C:\\Users\\ryosuke-ku\\Desktop\\' + project_name + '\\' + testProjectName + '\\**\\' + fileName , recursive=True)
+        if len(prodPath1)==0:
+            pass
+        else:
+            productionPath1.append(prodPath1[0])
+    return productionPath1
+
+def printTestPath(project_name):
+    initTestPath1 = glob.glob('C:\\Users\\ryosuke-ku\\Desktop\\' + project_name + '\\**\\*Test.java', recursive=True)
+    testPath1 = []
+    for initTestPath in initTestPath1:
+        testProjectName = re.sub(r"C:\\Users\\ryosuke-ku\\Desktop\\", "", initTestPath)[re.sub(r"C:\\Users\\ryosuke-ku\\Desktop\\", "", initTestPath).find("\\") + 1:][:re.sub(r"C:\\Users\\ryosuke-ku\\Desktop\\", "", initTestPath)[re.sub(r"C:\\Users\\ryosuke-ku\\Desktop\\", "", initTestPath).find("\\") + 1:].find('\\')]
+        # print(testProjectName)
+        testFileName = initTestPath[initTestPath.rfind("\\") + 1:]
+        fileName = testFileName.replace('Test','').replace('test','')
+        prodPath1 = glob.glob('C:\\Users\\ryosuke-ku\\Desktop\\' + project_name + '\\' + testProjectName + '\\**\\' + fileName , recursive=True)
+        if len(prodPath1)==0:
+            pass
+        else:
+            testPath1.append(initTestPath)
+    return testPath1
+
+if __name__ == '__main__':
+    utility_allPath = printProductionPath('utility')
+
+    for utility_path in utility_allPath:
+        print(utility_path)
+        utility_cut = re.sub(r"C:\\Users\\ryosuke-ku\\Desktop\\utility\\", "", utility_path)
+        utility_dir = utility_cut[:utility_cut.rfind('\\')]
+
+        print(utility_dir)
+        os.makedirs('systems\\' + utility_dir, exist_ok=True)
+        file = open('systems\\' + utility_cut,'w')
